1d8766a0cf2859181668270f39d3b3f266635fb0
Sumrish Hanif <sumrish@youcloudtech.com>
2023-03-08 21:55:22 -0330

    Upload forgot password
    Upload forgot password

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
+import { SessionProvider } from "next-auth/react";
+
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+  return (
+    <SessionProvider session={session}>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+    </SessionProvider>
+  )
+}
+
+export default MyApp
